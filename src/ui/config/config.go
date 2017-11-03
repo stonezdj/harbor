cda51c014ac9772f544fd09b462e6e7b051062fe
stonezdj <stonezdj@gmail.com>
2017-11-03 17:17:49 +0800

    Add unit test for ldap verify cert

--- src/ui/config/config.go
+++ src/ui/config/config.go
@@ -173,7 +173,6 @@ func LDAP() (*models.LDAP, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	ldap := &models.LDAP{}
 	ldap.URL = cfg[common.LDAPURL].(string)
 	ldap.SearchDN = cfg[common.LDAPSearchDN].(string)
@@ -183,7 +182,11 @@ func LDAP() (*models.LDAP, error) {
 	ldap.Filter = cfg[common.LDAPFilter].(string)
 	ldap.Scope = int(cfg[common.LDAPScope].(float64))
 	ldap.Timeout = int(cfg[common.LDAPTimeout].(float64))
-	ldap.VerifyCert = cfg[common.LDAPVerifyCert].(bool)
+	if cfg[common.LDAPVerifyCert] != nil {
+		ldap.VerifyCert = cfg[common.LDAPVerifyCert].(bool)
+	} else {
+		ldap.VerifyCert = false
+	}
 
 	return ldap, nil
 }
