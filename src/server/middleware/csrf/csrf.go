ac92a611ea1802c6f27a1a9860c2ea99bfdc3df9
stonezdj <stonezdj@gmail.com>
2021-09-05 16:02:47 +0800

    http failed

--- src/server/middleware/csrf/csrf.go
+++ src/server/middleware/csrf/csrf.go
@@ -30,6 +30,7 @@ var (
 // attachToken makes sure if csrf generate a new token it will be included in the response header
 func attachToken(w http.ResponseWriter, r *http.Request) {
 	if t := csrf.Token(r); len(t) > 0 {
+		log.Errorf("csrf token found %v", t)
 		w.Header().Set(tokenHeader, t)
 	} else {
 		log.Warningf("token not found in context, skip attaching")
