858fd2bf4a7a3dea886be189cfabf005ccc357f5
He Weiwei <hweiwei@vmware.com>
2022-02-19 13:33:46 +0000

    feat: merge local and remote tags for repo of proxy cache project in list API
    Signed-off-by: He Weiwei <hweiwei@vmware.com>

--- src/server/registry/util/util.go
+++ src/server/registry/util/util.go
@@ -1,10 +1,17 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"net/url"
 	"sort"
 	"strconv"
+	"strings"
+
+	"github.com/goharbor/harbor/src/lib/errors"
+	libhttp "github.com/goharbor/harbor/src/lib/http"
+	"github.com/goharbor/harbor/src/server/router"
 )
 
 // SetLinkHeader ...
@@ -35,4 +42,112 @@ func IndexString(strs []string, x string) int {
 		return i
 	}
 	return -1
+}
+
+const emptyN = -1
+
+// ParseNAndLastParameters parse the n and last parameters from the query of the http request
+func ParseNAndLastParameters(r *http.Request) (int, string, error) {
+	q := r.URL.Query()
+
+	n := emptyN
+
+	if q.Get("n") != "" {
+		value, err := strconv.Atoi(q.Get("n"))
+		if err != nil || value < 0 {
+			return 0, "", errors.New(err).WithCode(errors.BadRequestCode).WithMessage("the N must be a positive int type")
+		}
+
+		n = value
+	}
+
+	return n, q.Get("last"), nil
+}
+
+// SendListTagsResponse sends the response for list tags API
+func SendListTagsResponse(w http.ResponseWriter, r *http.Request, tags []string) {
+	n, last, err := ParseNAndLastParameters(r)
+	if err != nil {
+		libhttp.SendError(w, err)
+		return
+	}
+
+	items, nextLast := pickItems(sortedAndUniqueItems(tags), n, last)
+
+	if nextLast != "" {
+		link, err := SetLinkHeader(r.URL.String(), n, nextLast)
+		if err != nil {
+			libhttp.SendError(w, err)
+			return
+		}
+
+		w.Header().Set("Link", link)
+	}
+
+	body := struct {
+		Name string   `json:"name"`
+		Tags []string `json:"tags"`
+	}{
+		Name: router.Param(r.Context(), ":splat"),
+		Tags: items,
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if err := json.NewEncoder(w).Encode(body); err != nil {
+		libhttp.SendError(w, err)
+	}
+}
+
+func sortedAndUniqueItems(items []string) []string {
+	n := len(items)
+	if n <= 1 {
+		return items[0:n]
+	}
+
+	sort.Strings(items)
+
+	j := 1
+	for i := 1; i < n; i++ {
+		if items[i] != items[i-1] {
+			items[j] = items[i]
+			j++
+		}
+	}
+
+	return items[0:j]
+}
+
+func pickItems(tags []string, n int, last string) ([]string, string) {
+	if len(tags) == 0 || n == 0 {
+		return []string{}, ""
+	}
+
+	if n == emptyN {
+		n = len(tags)
+	}
+
+	i := 0
+	if last != "" {
+		lastIndex := sort.Search(len(tags), func(i int) bool { return strings.Compare(tags[i], last) >= 0 })
+		if tags[lastIndex] == last {
+			i = lastIndex + 1
+		} else {
+			i = lastIndex
+		}
+	}
+
+	j := i + n
+
+	if j >= len(tags) {
+		j = len(tags)
+	}
+
+	result := tags[i:j]
+
+	nextLast := ""
+	if len(result) > 0 && tags[len(tags)-1] != result[len(result)-1] {
+		nextLast = result[len(result)-1]
+	}
+
+	return result, nextLast
 }
--- src/server/registry/util/util_test.go
+++ src/server/registry/util/util_test.go
@@ -1,8 +1,10 @@
 package util
 
 import (
-	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateLinkEntry(t *testing.T) {
@@ -26,4 +28,182 @@ func TestIndexString(t *testing.T) {
 
 	assert.True(t, IndexString(a, "Z") == -1)
 	assert.True(t, IndexString(a, "") == -1)
+}
+
+func Test_pickItems(t *testing.T) {
+	type args struct {
+		tags []string
+		n    int
+		last string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  []string
+		want1 string
+	}{
+		{
+			"no parameters",
+			args{[]string{"a", "b", "c", "d"}, emptyN, ""},
+			[]string{"a", "b", "c", "d"},
+			"",
+		},
+		{
+			"n=0",
+			args{[]string{"a", "b", "c", "d"}, 0, ""},
+			[]string{},
+			"",
+		},
+		{
+			"n=1",
+			args{[]string{"a", "b", "c", "d"}, 1, ""},
+			[]string{"a"},
+			"a",
+		},
+		{
+			"n=2",
+			args{[]string{"a", "b", "c", "d"}, 2, ""},
+			[]string{"a", "b"},
+			"b",
+		},
+		{
+			"n=4", // n is the count of tags
+			args{[]string{"a", "b", "c", "d"}, 4, ""},
+			[]string{"a", "b", "c", "d"},
+			"",
+		},
+		{
+			"n=5", // n is bigger than the count of tags
+			args{[]string{"a", "b", "c", "d"}, 5, ""},
+			[]string{"a", "b", "c", "d"},
+			"",
+		},
+		{
+			"last=a",
+			args{[]string{"a", "b", "c", "d"}, emptyN, "a"},
+			[]string{"b", "c", "d"},
+			"",
+		},
+		{
+			"last=d",
+			args{[]string{"a", "b", "c", "d"}, emptyN, "d"},
+			[]string{},
+			"",
+		},
+		{
+			"n=1 last=a",
+			args{[]string{"a", "b", "c", "d"}, 1, "a"},
+			[]string{"b"},
+			"b",
+		},
+		{
+			"n=2 last=a",
+			args{[]string{"a", "b", "c", "d"}, 2, "a"},
+			[]string{"b", "c"},
+			"c",
+		},
+		{
+			"n=3 last=a", // just the left n tags
+			args{[]string{"a", "b", "c", "d"}, 3, "a"},
+			[]string{"b", "c", "d"},
+			"",
+		},
+		{
+			"n=4 last=a", // left tags is less than n
+			args{[]string{"a", "b", "c", "d"}, 4, "a"},
+			[]string{"b", "c", "d"},
+			"",
+		},
+		{
+			"n=1 last=d", // last is the last element of the tags
+			args{[]string{"a", "b", "c", "d"}, 1, "d"},
+			[]string{},
+			"",
+		},
+		{
+			"last=v3", // last element not found
+			args{[]string{"v1", "v2", "v4", "v5"}, emptyN, "v3"},
+			[]string{"v4", "v5"},
+			"",
+		},
+		{
+			"one item",
+			args{[]string{"a"}, emptyN, ""},
+			[]string{"a"},
+			"",
+		},
+		{
+			"one item n=2",
+			args{[]string{"a"}, 2, ""},
+			[]string{"a"},
+			"",
+		},
+		{
+			"two items",
+			args{[]string{"a", "b"}, emptyN, ""},
+			[]string{"a", "b"},
+			"",
+		},
+		{
+			"three items",
+			args{[]string{"a", "b", "c"}, emptyN, ""},
+			[]string{"a", "b", "c"},
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := pickItems(tt.args.tags, tt.args.n, tt.args.last)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pickItems() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("pickItems() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_sortedAndUniqueItems(t *testing.T) {
+	type args struct {
+		items []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			"nil",
+			args{nil},
+			nil,
+		},
+		{
+			"no item",
+			args{[]string{}},
+			[]string{},
+		},
+		{
+			"one item",
+			args{[]string{"a"}},
+			[]string{"a"},
+		},
+		{
+			"duplicate items",
+			args{[]string{"a", "a", "a"}},
+			[]string{"a"},
+		},
+		{
+			"unordered and duplicate items",
+			args{[]string{"a", "c", "a", "b", "a"}},
+			[]string{"a", "b", "c"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedAndUniqueItems(tt.args.items); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedAndUniqueItems() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
