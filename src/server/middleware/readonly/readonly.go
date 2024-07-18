f926526763a1685d32e1f6f1c5ce5152e5a22e0a
stonezdj <stone.zhang@broadcom.com>
2024-07-18 17:52:16 +0800

    fix issue
    Signed-off-by: stonezdj <stone.zhang@broadcom.com>

--- src/server/middleware/readonly/readonly.go
+++ src/server/middleware/readonly/readonly.go
@@ -15,11 +15,8 @@
 package readonly
 
 import (
-	"io"
 	"net/http"
 
-	"github.com/goharbor/harbor/src/common/security"
-	"github.com/goharbor/harbor/src/lib"
 	"github.com/goharbor/harbor/src/lib/config"
 	"github.com/goharbor/harbor/src/lib/errors"
 	lib_http "github.com/goharbor/harbor/src/lib/http"
@@ -47,18 +44,6 @@ var (
 		http.MethodOptions: true,
 	}
 )
-
-// ResponseWriter ...
-type ResponseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-// WriteHeader ...
-func (rw *ResponseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
-}
 
 // safeMethodSkipper returns false when the request method is safe methods
 func safeMethodSkipper(r *http.Request) bool {
@@ -88,33 +73,6 @@ func MiddlewareWithConfig(config Config, skippers ...middleware.Skipper) func(ht
 			lib_http.SendError(w, pkgE)
 			return
 		}
-		enableAudit := false
-		urlStr := r.URL.String()
-		username := "unknown"
-		var requestContent string
-		if r.Method == http.MethodPost || r.Method == http.MethodPut || r.Method == http.MethodDelete {
-			enableAudit = true
-			lib.NopCloseRequest(r)
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-				return
-			}
-			requestContent = string(body)
-			if secCtx, ok := security.FromContext(r.Context()); ok {
-				username = secCtx.GetUsername()
-			}
-		}
-		rw := &ResponseWriter{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		next.ServeHTTP(rw, r)
-		// if enableAudit {
-		// 	logger.Infof("the request user is %v", username)
-		// 	logger.Infof("the request Method is %v", r.Method)
-		// 	logger.Infof("the request URL is %v", urlStr)
-		// 	logger.Infof("the request body is %v", requestContent)
-		// }
+		next.ServeHTTP(w, r)
 	}, skippers...)
 }
