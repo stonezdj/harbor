ab6c023dead08957172db002b4a77057692068a6
stonezdj <stone.zhang@broadcom.com>
2024-10-31 17:43:32 +0800

    add user create event
    Signed-off-by: stonezdj <stone.zhang@broadcom.com>

--- src/controller/event/metadata/common_event.go
+++ src/controller/event/metadata/common_event.go
@@ -17,12 +17,84 @@ package metadata
 import (
 	"context"
 	"net/http"
-	"strings"
+	"regexp"
 	"time"
 
 	event2 "github.com/goharbor/harbor/src/controller/event"
 	"github.com/goharbor/harbor/src/pkg/notifier/event"
 )
+
+type funcResolve func(*CommonEventMetadata, *event.Event) error
+
+var url2Operation = map[string]funcResolve{
+	`/api\/v2\.0\/configurations$`: ResolveConfigureEvent,
+	`/c\/login$`:                   ResolveLoginEvent,
+	`/api\/v2\.0\/users$`:          ResolveUserEvent,
+}
+
+func ResolveConfigureEvent(ce *CommonEventMetadata, event *event.Event) error {
+	data := &event2.CommonEvent{}
+	data.Operation = "configuration"
+	data.Operator = ce.Username
+	data.ResourceName = "configuration"
+	data.SourceIP = ce.IPAddress
+	data.Payload = ce.RequestPayload
+	data.OcurrAt = time.Now()
+	data.OperationDescription = "change configuration"
+	data.OperationResult = "success"
+	if ce.ResponseCode != http.StatusOK {
+		data.OperationResult = "failed"
+	}
+	event.Topic = event2.TopicCommonEvent
+	event.Data = data
+	return nil
+}
+
+func ResolveLoginEvent(ce *CommonEventMetadata, event *event.Event) error {
+	if ce.RequestMethod != http.MethodPost {
+		return nil
+	}
+	data := &event2.CommonEvent{}
+	data.Operation = "login"
+	data.Operator = ce.Username
+	data.ResourceName = "user"
+	data.SourceIP = ce.IPAddress
+	data.Payload = ce.RequestPayload
+	data.OcurrAt = time.Now()
+	data.OperationDescription = "login"
+	data.OperationResult = "success"
+	if ce.ResponseCode != http.StatusOK {
+		data.OperationResult = "failed"
+	}
+	event.Topic = event2.TopicCommonEvent
+	event.Data = data
+	return nil
+}
+
+func ResolveUserEvent(ce *CommonEventMetadata, event *event.Event) error {
+	if ce.RequestMethod != http.MethodPost && ce.RequestMethod != http.MethodPut {
+		return nil
+	}
+	data := &event2.CommonEvent{}
+	data.Operation = "user"
+	data.Operator = ce.Username
+	data.ResourceName = "user"
+	data.SourceIP = ce.IPAddress
+	data.Payload = ce.RequestPayload
+	data.OcurrAt = time.Now()
+	if ce.RequestMethod == http.MethodPost {
+		data.OperationDescription = "create user"
+	} else {
+		data.OperationDescription = "update user"
+	}
+	data.OperationResult = "success"
+	if ce.ResponseCode != http.StatusCreated && ce.ResponseCode != http.StatusOK {
+		data.OperationResult = "failed"
+	}
+	event.Topic = event2.TopicCommonEvent
+	event.Data = data
+	return nil
+}
 
 // CommonEventMetadata used to record an API event related information
 type CommonEventMetadata struct {
@@ -43,21 +115,11 @@ type CommonEventMetadata struct {
 
 // Resolve parse the audit information from CommonEventMetadata
 func (c *CommonEventMetadata) Resolve(event *event.Event) error {
-	data := &event2.CommonEvent{}
-	if strings.HasSuffix(c.RequestURL, "api/v2.0/configurations") {
-		data.Operation = "configuration"
-		data.Operator = c.Username
-		data.ResourceName = "configuration"
-		data.SourceIP = c.IPAddress
-		data.Payload = c.RequestPayload
-		data.OcurrAt = time.Now()
-		data.OperationDescription = "change configuration"
-		data.OperationResult = "success"
-		if c.ResponseCode != http.StatusOK {
-			data.OperationResult = "failed"
+	for url, resolve := range url2Operation {
+		p := regexp.MustCompile(url)
+		if p.MatchString(c.RequestURL) {
+			return resolve(c, event)
 		}
 	}
-	event.Topic = event2.TopicCommonEvent
-	event.Data = data
 	return nil
 }
