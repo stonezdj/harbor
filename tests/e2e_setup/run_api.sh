76fd6a8a3ed3247d5b8f34ca299f4517818606ea
stonezdj <stonezdj@gmail.com>
2025-05-06 09:47:25 +0000

    fix error
    Signed-off-by: stonezdj <stonezdj@gmail.com>

--- tests/e2e_setup/run_api.sh
+++ tests/e2e_setup/run_api.sh
@@ -2,5 +2,5 @@
 
 /usr/local/bin/containerd > ./daemon-local.log 2>&1 &
 /usr/bin/dockerd --insecure-registry=0.0.0.0/0 >./daemon-local.log 2>&1 &
-rm -rf /drone/tests/e2e_setup/run_api.sh
+rm -rf /drone/test-reports/*
 SWAGGER_CLIENT_PATH=/drone/harborclient/harbor_v2_swagger_client HARBOR_HOST=zdj-dev.local PYTHONPATH=/drone/tests/apitests/python python /drone/tests/apitests/python/api.py && junit-viewer --results=/drone/test-reports --save=/drone/test-reports/report.html
