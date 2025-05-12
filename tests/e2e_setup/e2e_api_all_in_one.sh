9050f9adeb1b872a1d1685075152da58761cd482
stonezdj <stonezdj@gmail.com>
2025-05-12 09:46:56 +0000

    fix it
    Signed-off-by: stonezdj <stonezdj@gmail.com>

--- tests/e2e_setup/e2e_api_all_in_one.sh
+++ tests/e2e_setup/e2e_api_all_in_one.sh
@@ -2,4 +2,4 @@
 set -e
 E2E_IMAGE="firstfloor/harbor-e2e-engine:api-slim"
 DIR=${PWD}
-docker run -i --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -v /etc/hosts:/etc/hosts -e HARBOR_HOST=${HARBOR_HOST} -e https_proxy=${https_proxy} -e http_proxy=${http_proxy} -w /drone ${E2E_IMAGE} bash -x /app/run_api.sh
+docker run -i --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -v /etc/hosts:/etc/hosts -e HARBOR_HOST=http://${HARBOR_HOST} -e https_proxy=http://${https_proxy} -e http_proxy=http://${http_proxy} -w /drone ${E2E_IMAGE} bash -x /app/run_api.sh
