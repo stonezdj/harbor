52d1d4c483f5de4d284fa6f07a529dff017b9ee2
stonezdj <stonezdj@gmail.com>
2025-05-12 09:50:16 +0000

    fix
    Signed-off-by: stonezdj <stonezdj@gmail.com>

--- tests/e2e_setup/e2e_api_container.sh
+++ tests/e2e_setup/e2e_api_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HARBOR_HOST=zdj-dev.local
+# HARBOR_HOST=zdj-dev.local
 E2E_IMAGE="firstfloor/harbor-e2e-engine:api-slim"
 DIR=${PWD}
-docker run -it --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -v /etc/hosts:/etc/hosts -w /drone $E2E_IMAGE sh
+docker run -it --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -v /etc/hosts:/etc/hosts -e https_proxy=http://${https_proxy} -e http_proxy=http://${http_proxy}  -w /drone $E2E_IMAGE sh
