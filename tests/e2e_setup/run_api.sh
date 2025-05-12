e3422405be5924504596056d5af2c044f6d4a842
stonezdj <stonezdj@gmail.com>
2025-05-12 08:01:52 +0000

    update run_api.sh
    Signed-off-by: stonezdj <stonezdj@gmail.com>

--- tests/e2e_setup/run_api.sh
+++ tests/e2e_setup/run_api.sh
@@ -3,6 +3,7 @@
 /usr/local/bin/containerd > ./daemon-local.log 2>&1 &
 /usr/bin/dockerd --insecure-registry=0.0.0.0/0 >./daemon-local.log 2>&1 &
 rm -rf /drone/test-reports/*
+make swagger_client
 SWAGGER_CLIENT_PATH=/drone/harborclient/harbor_v2_swagger_client PYTHONPATH=/drone/tests/apitests/python python /app/api.py 
 exit_code=$?
 junit-viewer --results=/drone/test-reports --save=/drone/test-reports/report.html
