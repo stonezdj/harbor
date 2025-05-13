0598fc04d2b8bfd452891a458234911f67223bc1
stonezdj <stonezdj@gmail.com>
2025-05-13 09:53:52 +0000

    fix env issue
    Signed-off-by: stonezdj <stonezdj@gmail.com>

--- tests/e2e_setup/e2e_api_container.sh
+++ tests/e2e_setup/e2e_api_container.sh
@@ -4,4 +4,4 @@
 E2E_IMAGE="firstfloor/harbor-e2e-engine:api-slim"
 DIR=${PWD}
 wget --no-check-certificate https://${HARBOR_HOST}/api/v2.0/systeminfo/getcert -O ca.crt
-docker run -it --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -v -v ${PWD}/ca.crt:/etc/docker/certs.d/${HARBOR_HOST}/ca.crt -v /etc/hosts:/etc/hosts -e https_proxy=http://${https_proxy} -e http_proxy=http://${http_proxy}  -w /drone $E2E_IMAGE sh
+docker run -it --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -v ${PWD}/ca.crt:/etc/docker/certs.d/${HARBOR_HOST}/ca.crt -v /etc/hosts:/etc/hosts -e HARBOR_HOST=${HARBOR_HOST} -e https_proxy=http://${https_proxy} -e http_proxy=http://${http_proxy}  -w /drone $E2E_IMAGE sh
