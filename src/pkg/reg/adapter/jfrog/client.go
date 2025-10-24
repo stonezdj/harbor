b6a9fb55e253a23e6c999659af7b709238509c20
stonezdj <stone.zhang@broadcom.com>
2025-10-24 14:37:57 +0800

    Add oci type support for jfrog registry
    Signed-off-by: stonezdj <stone.zhang@broadcom.com>

--- src/pkg/reg/adapter/jfrog/client.go
+++ src/pkg/reg/adapter/jfrog/client.go
@@ -58,10 +58,10 @@ func newClient(reg *model.Registry) *client {
 	}
 }
 
-// getDockerRepositories gets docker repositories from jfrog
-func (c *client) getDockerRepositories() ([]*repository, error) {
+// getRepositories gets docker repositories from jfrog
+func (c *client) getRepositories(packageType string) ([]*repository, error) {
 	var repositories []*repository
-	url := fmt.Sprintf("%s/artifactory/api/repositories?packageType=docker", c.url)
+	url := fmt.Sprintf("%s/artifactory/api/repositories?packageType=%s", c.url, packageType)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return repositories, err
