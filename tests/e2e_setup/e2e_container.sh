ee52d7806dc9328959a732d6f0f9efd414d17515
stonezdj <stone.zhang@broadcom.com>
2026-07-02 07:01:47 +0000

    update e2e_container to pass testcase in the shell script arg
    Signed-off-by: stonezdj <stone.zhang@broadcom.com>

--- tests/e2e_setup/e2e_container.sh
+++ tests/e2e_setup/e2e_container.sh
@@ -3,6 +3,12 @@
 HARBOR_SRC_FOLDER=$(realpath ../../)
 echo ${HARBOR_SRC_FOLDER}
 
+testcase_tag=$1
+
 # If the testbed network type is private need to set NETWORK_TYPE private, default is public
-docker run -it --privileged -v /var/log/harbor:/var/log/harbor -v /etc/hosts:/etc/hosts -v ${HARBOR_SRC_FOLDER}:/drone -v ${HARBOR_SRC_FOLDER}/tests/harbor_ca.crt:/ca/ca.crt -v /dev/shm:/dev/shm -e NETWORK_TYPE=public -e PIP_INDEX_URL=https://packages.vcfd.broadcom.net/artifactory/api/pypi/pypi-virtual/simple -w /drone registry.goharbor.io/harbor-ci/goharbor/harbor-e2e-engine:latest-ui /bin/bash
+if [ -z "${testcase_tag}" ]; then
+    docker run -it --privileged -v /var/log/harbor:/var/log/harbor -v /etc/hosts:/etc/hosts -v ${HARBOR_SRC_FOLDER}:/drone -v ${HARBOR_SRC_FOLDER}/tests/harbor_ca.crt:/ca/ca.crt -v /dev/shm:/dev/shm -e NETWORK_TYPE=public -e PIP_INDEX_URL=https://packages.vcfd.broadcom.net/artifactory/api/pypi/pypi-virtual/simple -w /drone registry.goharbor.io/harbor-ci/goharbor/harbor-e2e-engine:latest-ui /bin/bash
+else
+    docker run  --privileged -v /var/log/harbor:/var/log/harbor -v /etc/hosts:/etc/hosts -v ${HARBOR_SRC_FOLDER}:/drone -v ${HARBOR_SRC_FOLDER}/tests/harbor_ca.crt:/ca/ca.crt -v /dev/shm:/dev/shm -e NETWORK_TYPE=public -e PIP_INDEX_URL=https://packages.vcfd.broadcom.net/artifactory/api/pypi/pypi-virtual/simple -w /drone registry.goharbor.io/harbor-ci/goharbor/harbor-e2e-engine:latest-ui /drone/tests/e2e_setup/single_test.sh ${testcase_tag}
+fi
 
